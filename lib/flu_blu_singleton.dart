d2f58991f90be5b09585e2427eb2b676470ef19b
adarshs <adarshs@meditab.com>
2023-02-15 16:21:46 +0530

    -- Copyright removed.

--- lib/flu_blu_singleton.dart
+++ lib/flu_blu_singleton.dart
@@ -3,29 +3,6 @@ import 'dart:io';
 import 'package:flutter_blue_plus/flutter_blue_plus.dart';
 import 'package:permission_handler/permission_handler.dart';
 
-/// Meditab Software Inc. CONFIDENTIAL
-///__________________
-///
-/// [2023] Meditab Software Inc.
-/// All Rights Reserved.
-///
-/// NOTICE:  All information contained herein is, and remains
-/// the property of Meditab Software Inc. and its suppliers,
-/// if any.  The intellectual and technical concepts contained
-/// herein are proprietary to Meditab Software Incorporated
-/// and its suppliers and may be covered by U.S. and Foreign Patents,
-/// patents in process, and are protected by trade secret or copyright law.
-/// Dissemination of this information or reproduction of this material
-/// is strictly forbidden unless prior written permission is obtained
-/// from Meditab Software Incorporated.
-///
-/// File Name: flu_blu_singleton
-///
-/// @author Adarsh Sudarsanan (adarshs@meditab.com) Meditab Software Inc.
-/// @version 1.0.0
-/// @since 15/02/23
-///
-
 typedef FluBluScanController = StreamController<ScanResult>;
 
 enum FluBluStatus { unavailable, on, denied, accepted }
